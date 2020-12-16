2616b288be40f95ce4a59d58f07a57b5f2955f7a
gyu <cho2304@naver.com>
2020-12-16 13:44:09 +0900

    4. render movie api and styling at application

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,83 @@
+import axios from 'axios';
 import React from 'react';
-
-interface Props {
-
-}
+import "./Movie.css";
 
 interface State {
-    count: number;
+    isLoading?: boolean;
+    movies?: any
 }
 
-class App extends React.Component<Props, State> {
+function Movie(movie: MovieProps) {
+    return <div className="movie">
+        <img src={movie.medium_cover_image} alt={movie.title} title={movie.title}/>
+        <div className="movie__data">
+            <h3 className="movie__title">{movie.title}</h3>
+            <h5 className="movie__year">{movie.year}</h5>
+            <ul className="movie__genres">{movie.genres.map((genre: string) =>
+                <li className="genres__genre">
+                    {genre}
+                </li>
+            )}
+            </ul>
+            <p className="movie__summary">{movie.summary.slice(0,180)}...</p>
+        </div>
+    </div>;
+}
+
+class MovieProps {
+    id: number;
+    year: number;
+    title: string;
+    summary: string;
+    medium_cover_image: string;
+    genres: Array<string>;
+
+    constructor(id: number, year: number, title: string, summary: string, medium_cover_image: string, genres:Array<string>) {
+        this.id = id;
+        this.year = year;
+        this.title = title;
+        this.summary = summary;
+        this.medium_cover_image = medium_cover_image;
+        this.genres = genres;
+    }
+}
+
+class App extends React.Component<State> {
     state: State = {
-        count: 0
+        isLoading: true,
+        movies: []
     }
 
-    add = ():void => {
-        this.setState(
-            ({count}) => ({count: count + 1}))
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies }
+            }
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+        this.setState({movies: movies, isLoading: false});
     }
-
-    minus = ():void => {
-        this.setState(
-            ({count}) => ({count: count - 1})
-        )
-    };
 
     componentDidMount() {
-        console.log("component rendered")
-    }
-
-    componentDidUpdate() {
-        console.log("component updated")
-    }
-
-    componentWillUnmount() {
-        console.log("Good bye")
+        this.getMovies();
     }
 
     render() {
-        console.log("I'm rendering...")
-        return (
-            <div className="App">
-                <h1>The number is : {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-        );
+        const { isLoading, movies } = this.state;
+        return <section className="container">
+            {isLoading ?
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                </div> : (
+                    <div className="movies">
+                        {
+                            movies.map((movie: MovieProps) => (
+                                <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title}
+                                       summary={movie.summary} medium_cover_image={movie.medium_cover_image} genres={movie.genres}/>
+                            ))
+                        }
+                    </div>
+                )}
+        </section>;
     }
 }
 
